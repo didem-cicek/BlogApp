937357d8733719d01ed6cf5823cd9aeafcd3f8cd
didem-cicek <cicek.didem35@gmail.com>
2022-08-09 01:28:13 +0300

    08.08.2022 - Tema Giydirme

---
+++ BlogApp/BlogApp/Controllers/BlogController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Single() => View();
+    }
+}
